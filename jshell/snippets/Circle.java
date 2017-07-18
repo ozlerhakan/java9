8537992b9d44d5059c4b19ae1a5e7cf2a6a21f29
Hakan Ozler <ozler.hakan@gmail.com>
2017-07-18 19:26:35 +0300

    add the second part

---
+++ jshell/snippets/Circle.java
@@ -0,0 +1,9 @@
+class Circle extends Figure {
+    final double radius;
+    Circle(double radius) {
+        this.radius = radius;
+    }
+    double area() {
+        return Math.PI * (radius * radius);
+    }
+}
---
+++ jshell/snippets/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle extends Figure {
+    final double length;
+    final double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    double area() {
+        return length * width;
+    }
+}
